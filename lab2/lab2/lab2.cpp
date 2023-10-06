1b151a5b13a17d1e01a45249d242b57234340141
sqrd.max <sqrd.max@gmail.com>
2023-10-06 05:18:37 +0300

    add lab2

---
+++ lab2/lab2/lab2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+    int i = 69;
+    int number = 72;
+    
+    i = 69 + 72;
+    
+    int group[3] = {0, 0, 0};
+    
+    int* iPointer = &i;
+    
+    number = *iPointer;
+    
+    *iPointer = 15;
+    
+    iPointer = &group[0];
+    iPointer += 2; // Перемещаем указатель на 3 элемент массива
+    
+    group[0] = 5;
+    *(iPointer) = 6;
+
+    std::cout << "i: " << i << std::endl;
+    std::cout << "number: " << number << std::endl;
+    std::cout << "group[0]: " << group[0] << std::endl;
+    std::cout << "group[2]: " << group[2] << std::endl;
+
+    return 0;
+}
